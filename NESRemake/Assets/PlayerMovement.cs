fda43a4ff88d43d0380c42230455e5254110cb11
Krystal Grant <kgrant@utc4me.org>
2016-01-07 10:29:56 -0500

    Commit #2
    Started working on player controls and realized moving on my isometric
    grid will be a lot trickier than I thought. I think once I get that all
    sorted out everything else will fall into place for sure.

---
+++ NESRemake/Assets/PlayerMovement.cs
@@ -0,0 +1,36 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlayerMovement : MonoBehaviour {
+
+	[SerializeField] float speed;
+	// [SerializeField] Transform[] points;
+	
+	float h;
+	float v;
+	//Rigidbody player;
+	Vector3 movement;
+	
+	void Awake ()
+	{
+		//player = GetComponent<Rigidbody>();
+	}
+	
+	void Update ()
+	{
+		h = Input.GetAxisRaw("Horizontal");
+		v = Input.GetAxisRaw("Vertical");
+		
+		Move (h, v);
+	}
+	
+	void Move (float h, float v)
+	{
+		Vector3 current = transform.position;
+//		movement.Set ( v, h, -h);
+//		movement = movement.normalized * speed * Time.deltaTime;
+		transform.position = new Vector3 (Mathf.Round(current.x * v),
+			Mathf.Round(current.y * h),
+			Mathf.Round(current.z * -h));
+	}
+}
