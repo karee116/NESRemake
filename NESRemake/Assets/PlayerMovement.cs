8e74e7e7584f9749dd08aa43065b1e01c4b066df
Krystal Grant <kgrant@utc4me.org>
2016-01-08 10:29:37 -0500

    Commit #3
    I got it to move! (kinda....)

--- NESRemake/Assets/PlayerMovement.cs
+++ NESRemake/Assets/PlayerMovement.cs
@@ -6,13 +6,14 @@ public class PlayerMovement : MonoBehaviour {
 	[SerializeField] float speed;
 	// [SerializeField] Transform[] points;
 	
-	float h;
-	float v;
+	public float h;
+	public float v;
 	//Rigidbody player;
-	Vector3 movement;
+	Vector3 current;
 	
 	void Awake ()
 	{
+		current = transform.position;
 		//player = GetComponent<Rigidbody>();
 	}
 	
@@ -21,16 +22,45 @@ public class PlayerMovement : MonoBehaviour {
 		h = Input.GetAxisRaw("Horizontal");
 		v = Input.GetAxisRaw("Vertical");
 		
+		
 		Move (h, v);
+	}
+	
+	void LateUpdate ()
+	{
 	}
 	
 	void Move (float h, float v)
 	{
-		Vector3 current = transform.position;
+	
+		if (Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.DownArrow))
+		{
+			transform.position = new Vector3 (Mathf.Round(transform.position.x + h), Mathf.Round(transform.position.y + v),
+				Mathf.Round(transform.position.z));
+				
+		}
+		else if (Input.GetKey(KeyCode.LeftArrow) && Input.GetKey(KeyCode.UpArrow))
+		{
+			transform.position = new Vector3 (Mathf.Round(transform.position.x + h), Mathf.Round(transform.position.y + v),
+			    Mathf.Round(transform.position.z));
+		}
+		
+		else if (Input.GetKey(KeyCode.RightArrow) && Input.GetKey(KeyCode.DownArrow))
+		{
+			transform.position = new Vector3 (Mathf.Round(transform.position.x), Mathf.Round(transform.position.y + v),
+			    Mathf.Round(transform.position.z + h));
+		}
+		else if (Input.GetKey(KeyCode.RightArrow) && Input.GetKey(KeyCode.UpArrow))
+		{
+			transform.position = new Vector3 (Mathf.Round(transform.position.x), Mathf.Round(transform.position.y + v),
+			    Mathf.Round(transform.position.z + h));
+		}
 //		movement.Set ( v, h, -h);
 //		movement = movement.normalized * speed * Time.deltaTime;
-		transform.position = new Vector3 (Mathf.Round(current.x * v),
-			Mathf.Round(current.y * h),
-			Mathf.Round(current.z * -h));
+//		transform.position = new Vector3 (Mathf.Round(current.x + -h),
+//			Mathf.Round(current.y + v),
+//			Mathf.Round(current.z + -v));
+		
+
 	}
 }
